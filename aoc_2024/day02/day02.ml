7cbd50560ab0b187d355539305e1f09b1de8aee9
TydraoXalys <quentin.lenfant63@gmail.com>
2024-12-09 00:47:00 +0100

    [Day 02] Part 2

--- aoc_2024/day02/day02.ml
+++ aoc_2024/day02/day02.ml
@@ -3,6 +3,16 @@
 (* ==================================== *)
 
 (* ==================================== *)
+(* PRINT FUNCTIONS                      *)
+(* ==================================== *)
+
+let print_result x =
+  begin
+    print_int x;
+    print_newline ();
+  end
+
+(* ==================================== *)
 (* COMMON FUNCTIONS                     *)
 (* ==================================== *)
 
@@ -14,7 +24,7 @@
   * - comparator : Indicates if levels must be all increasing (=1) or decreasing (=-1).
   *)
 let is_fst_rule_broken lvl1 lvl2 comparator =
-  (lvl1 > lvl2 && comparator = -1) || (lvl1 < lvl2 && comparator = 1) 
+  (lvl1 > lvl2 && comparator <= -1) || (lvl1 < lvl2 && comparator >= 1) 
 
 (** Checks if the second rule is broken.
   * NB: The second rule say that the difference between two adjacent levels must be between 1 and 3.
@@ -33,10 +43,6 @@ let is_snd_rule_broken lvl1 lvl2 =
   *)
 let are_rules_broken lvl1 lvl2 comparator =
   (is_fst_rule_broken lvl1 lvl2 comparator) || (is_snd_rule_broken lvl1 lvl2)
-
-let rec print_list l = match l with
-  | [] -> ()
-  | t :: q -> begin print_int t; print_char ' '; print_list q; end
 
 (* ==================================== *)
 (* PUZZLE PARSING                       *)
@@ -101,6 +107,19 @@ let rec count_safe reports = match reports with
 (* PART TWO                             *)
 (* ==================================== *)
 
+(** Gets the comparator that tells if levels must be all increasing or decreasing.
+  *
+  * Params:
+  * - report : List of integers that represent the report to analyse.
+  *)
+let get_comparator report =
+  let rec get_comparator_rec report acc = match report with
+    | [] -> 0
+    | _ :: [] -> 0
+    | _ when acc = 4 -> 0
+    | lvl1 :: lvl2 :: q -> (get_comparator_rec (lvl2::q) (acc+1)) + (compare lvl1 lvl2)
+  in get_comparator_rec report 0
+
 (** Execute the next iteration of the report analysis in Part Two.
   *
   * Params:
@@ -141,7 +160,10 @@ let rec next_iteration report previous_lvl comparator bad_lvl_found =
     (* Code of check_iteration() *)
     in if (
       bad_lvl_found || (
-        are_rules_broken previous_lvl lvl2 comparator && 
+        (
+          are_rules_broken previous_lvl lvl2 comparator ||
+          are_rules_broken lvl2 next_lvl comparator
+        ) && 
         are_rules_broken lvl1 next_lvl comparator
       )
     )
@@ -171,19 +193,21 @@ let is_safe_with_dampener report =
     * Params:
     * - lvl1, lvl2    : Levels that are analysed in this iteration.
     * - next_lvl      : Level that follows lvl2 in the entire report.
+    * - comparator    : Indicates if levels must be all increasing or decreasing.
     * - report        : List of integers that represent the remaining part of the report to analyse.
     *)
-  let check_first_iteration lvl1 lvl2 next_lvl report =
+  let check_first_iteration lvl1 lvl2 next_lvl comparator report =
 
     (*  Gets the value of previous_lvl for the second iteration.
       *
       * Params:
       * - lvl1, lvl2    : Levels that are analysed in this iteration.
       * - next_lvl      : Level that follows lvl2 in the entire report.
+      * - comparator    : Indicates if levels must be all increasing or decreasing.
       *)
-    let get_previous_level lvl1 lvl2 next =
+    let get_previous_level lvl1 lvl2 next_lvl comparator =
       if 
-        is_snd_rule_broken lvl1 next 
+        are_rules_broken lvl1 next_lvl comparator
       then 
         lvl2
       else 
@@ -191,21 +215,22 @@ let is_safe_with_dampener report =
     
     (* Code of check_first_iteration() *)
     in if 
-      is_snd_rule_broken lvl1 next_lvl && 
-      is_snd_rule_broken lvl2 next_lvl
+      are_rules_broken lvl1 next_lvl comparator && 
+      are_rules_broken lvl2 next_lvl comparator
     then 
       false
     else
-      let previous = get_previous_level lvl1 lvl2 next_lvl
-      in next_iteration (next_lvl::report) previous (compare previous next_lvl) true
+      let previous = get_previous_level lvl1 lvl2 next_lvl comparator
+      in next_iteration (next_lvl::report) previous comparator true
   
   (* Code of is_safe_with_dampener() *)
-  in match report with
-    | []                                                              -> failwith "Empty report"
-    | _ :: []                                                         -> true
-    | _ :: _ :: []                                                    -> true
-    | lvl1 :: lvl2 :: next_lvl :: q when is_snd_rule_broken lvl1 lvl2 -> check_first_iteration lvl1 lvl2 next_lvl q
-    | lvl1 :: lvl2 :: q                                               -> next_iteration (lvl2::q) lvl1 (compare lvl1 lvl2) false
+  in let comparator = get_comparator report
+  in if comparator = 0 then false else match report with
+    | []                                                                        -> failwith "Empty report"
+    | _ :: []                                                                   -> true
+    | _ :: _ :: []                                                              -> true
+    | lvl1 :: lvl2 :: next_lvl :: q when are_rules_broken lvl1 lvl2 comparator  -> check_first_iteration lvl1 lvl2 next_lvl comparator q
+    | lvl1 :: lvl2 :: q                                                         -> next_iteration (lvl2::q) lvl1 comparator false
 
 
 (** Counts the number of safe reports, taking into account The Problem Dampener (0 or 1 bad level). 
@@ -214,9 +239,9 @@ let is_safe_with_dampener report =
   * - reports : List that contains all reports.
   *)
 let rec count_safe_with_dampener reports = match reports with
-  | []                                             -> 0
-  | report :: q when is_safe_with_dampener report  -> 1 + count_safe_with_dampener q
-  | report :: q                                    -> begin print_list report; print_newline (); count_safe_with_dampener q; end
+  | []                                            -> 0
+  | report :: q when is_safe_with_dampener report -> 1 + count_safe_with_dampener q
+  | _ :: q                                        -> count_safe_with_dampener q
 
 (* ==================================== *)
 (* MAIN                                 *)
@@ -225,10 +250,8 @@ let rec count_safe_with_dampener reports = match reports with
 let () =
   let reports = read_file "./day02/day02.input" in 
   begin
-    print_int (count_safe reports);
-    print_newline ();
-    print_int (count_safe_with_dampener reports);
-    print_newline ();
+    print_result (count_safe reports);
+    print_result (count_safe_with_dampener reports);
   end
 
 (* =================================== *)
